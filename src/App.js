6ad3170ae2d7528d66d6d420e4f3febaa0c19774
jean2077 <jeajun9568@gmail.com>
2024-11-01 06:25:20 +0900

    로그인전 메인페이지 구현

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div class="ballon_left">안녕하세요! 오늘은 우리 把자문에 대해 배워볼까요? </div>
-      <p>혜으응,,,,,</p>
+      <div class="hello">안녕하세요! hello! 你好！</div>  
+      <div class="ballon_left">안녕하세요! 혜성학생
+        오늘은 우리 把자문에 대해 배워볼까요? </div>
+      
       <div class="polygon"></div>
       <div class="rectangle"></div>
       <div class="ballon_right">선생님 안녕하세요!</div>
       <div class="chat_ractangle"></div>
+      <div class="long_rectangle"></div>
+      <div class="long_in_rectangle">여기를 눌러 무료로 사용해보세요</div>
     </div>
   );
 }
