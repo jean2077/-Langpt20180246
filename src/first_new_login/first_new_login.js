f89574b13762c9cd023ba85f8e648cb8bc2c8dc6
jean2077 <jeajun9568@gmail.com>
2024-11-03 20:26:43 +0900

    처음 로그인 페이지 파일생성

---
+++ src/first_new_login/first_new_login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './first_new_login.css'; // 스타일 시트 임포트
+
+function firstNewLogin() {
+  return (
+    <div className="ChoiceMemType">
+
+      <div class="hello_3">안녕하세요! hello! 你好！</div>
+      <div class="chat_ractangle_3"></div>
+      <div class="new_member_button">회원가입</div>
+      
+      <button class="google-button2">구글로 간편로그인</button>
+      
+    
+    </div>
+  );
+}
+
+export default firstNewLogin;
