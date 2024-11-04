9b6e7310dc303b9bff4c951243e79c0e862bf925
jean2077 <jeajun9568@gmail.com>
2024-11-04 16:46:28 +0900

    작업 내용 저장

--- src/first_new_login/first_new_login.js
+++ src/first_new_login/first_new_login.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import './first_new_login.css'; // 스타일 시트 임포트
 
-function firstNewLogin() {
+function FirstNewLogin() {
   return (
-    <div className="ChoiceMemType">
-
-      <div class="hello_3">안녕하세요! hello! 你好！</div>
-      <div class="chat_ractangle_3"></div>
-      <div class="new_member_button">회원가입</div>
-      
-      <button class="google-button2">구글로 간편로그인</button>
-      
-    
+    <div className="FirstNewLogin">
+      <div className="hello_new_login">안녕하세요! hello! 你好！</div>
+      <div className="chat_ractangle"></div>
     </div>
   );
 }
 
-export default firstNewLogin;
+export default FirstNewLogin;
